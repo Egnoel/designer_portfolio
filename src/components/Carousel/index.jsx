42d0df326a250a0eb294aab751c06c73265c110e
Egnoel <70013986+Egnoel@users.noreply.github.com>
2023-09-04 20:11:28 +0100

    styling and new component

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -5,6 +5,8 @@ import slide2 from "../../assets/image-slide-2.jpg";
 import slide3 from "../../assets/image-slide-3.jpg";
 import slide4 from "../../assets/image-slide-4.jpg";
 import slide5 from "../../assets/image-slide-5.jpg";
+import icon_arrow_left from "../../assets/icon-arrow-left.svg";
+import icon_arrow_right from "../../assets/icon-arrow-right.svg";
 
 const Carousel = () => {
   const images = [slide1, slide2, slide3, slide4, slide5];
@@ -24,6 +26,7 @@ const Carousel = () => {
 
   return (
     <div className="carousel">
+      <h2>My Work</h2>
       <div className="image-container">
         {visibleImages.map((image, index) => (
           <img key={index} src={image} alt="slide" />
@@ -31,13 +34,13 @@ const Carousel = () => {
       </div>
       <div className="carousel_btn">
         <button onClick={goBack} disabled={currentIndex === 0}>
-          Previous
+          <img src={icon_arrow_left} alt="previous" />
         </button>
         <button
           onClick={goForward}
           disabled={currentIndex >= images.length - 3}
         >
-          Next
+          <img src={icon_arrow_right} alt="next" />
         </button>
       </div>
     </div>
