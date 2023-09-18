eca06bd81f25be92c9a24a549824bf16f96347bf
Egnoel <70013986+Egnoel@users.noreply.github.com>
2023-09-18 15:03:03 +0100

    media queries, tablet & phone mode

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Contact from "./components/Contact";
 import Logo from "./components/Logo";
 import Motto from "./components/Motto";
 import Skills from "./components/Skills";
-import Works from "./components/Works";
 
 function App() {
   return (
