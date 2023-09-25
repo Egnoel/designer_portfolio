21727dd99f8a61c4b45bba5359d076fa26348c60
Egnoel <70013986+Egnoel@users.noreply.github.com>
2023-09-25 21:09:40 +0100

    improving design

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,50 +1,97 @@
 import React, { useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./style.css";
-import slide1 from "../../assets/image-slide-7.jpg";
-import slide2 from "../../assets/image-slide-2.jpg";
-import slide3 from "../../assets/image-slide-3.jpg";
-import slide4 from "../../assets/image-slide-4.jpg";
-import slide5 from "../../assets/image-slide-5.jpg";
-import icon_arrow_left from "../../assets/icon-arrow-left.svg";
-import icon_arrow_right from "../../assets/icon-arrow-right.svg";
+import { Carousel } from "react-responsive-carousel";
+import slide1 from "../../assets/image-slide-2.jpg";
+import slide2 from "../../assets/image-slide-3.jpg";
+import slide3 from "../../assets/image-slide-4.jpg";
+import slide4 from "../../assets/image-slide-5.jpg";
+import slide5 from "../../assets/image-slide-7.jpg";
+import arrowLeft from "../../assets/icon-arrow-left.svg";
+import arrowRight from "../../assets/icon-arrow-right.svg";
 
-const Carousel = () => {
-  const images = [slide1, slide2, slide3, slide4, slide5];
-  const [currentIndex, setCurrentIndex] = useState(0);
+// Other imports and styles...
 
-  const goBack = () => {
-    const newIndex = currentIndex - 3;
-    setCurrentIndex(newIndex < 0 ? 0 : newIndex);
+const CenterModeCarousel = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [autoPlay, setAutoPlay] = useState(true);
+
+  const next = () => {
+    setCurrentSlide((prevSlide) => prevSlide + 1);
   };
 
-  const goForward = () => {
-    const newIndex = currentIndex + 3;
-    setCurrentIndex(newIndex >= images.length ? images.length - 3 : newIndex);
+  const prev = () => {
+    setCurrentSlide((prevSlide) => prevSlide - 1);
   };
 
-  const visibleImages = images.slice(currentIndex, currentIndex + 3);
+  const updateCurrentSlide = (index) => {
+    if (currentSlide !== index) {
+      setCurrentSlide(index);
+    }
+  };
+  const createCarouselItemImage = (index, options = {}) => (
+    <div key={index} className="img-container">
+      <img
+        src={index}
+        alt="carousel"
+        style={{
+          maxWidth: "100%", // Set maximum width to ensure responsiveness
+          maxHeight: "100%", // Set maximum height to ensure responsiveness
+          objectFit: "cover", // Set how the image is fitted inside the container
+        }}
+      />
+    </div>
+  );
+
+  const getConfigurableProps = () => ({
+    showStatus: false,
+    showIndicators: false,
+    showArrows: false,
+    infiniteLoop: true,
+    showThumbs: false,
+    useKeyboardArrows: true,
+    autoPlay: true,
+    stopOnHover: true,
+    swipeable: true,
+    dynamicHeight: true,
+    emulateTouch: true,
+    autoFocus: false,
+    thumbWidth: 100,
+    selectedItem: 0,
+    interval: 2000,
+    transitionTime: 500,
+    swipeScrollTolerance: 5,
+  });
+
+  const baseChildren = (
+    <div>
+      {[slide1, slide2, slide3, slide4, slide5].map(createCarouselItemImage)}
+    </div>
+  );
 
   return (
-    <div className="carousel">
-      <h2>My Work</h2>
-      <div className="image-container">
-        {visibleImages.map((image, index) => (
-          <img key={index} src={image} alt="slide" />
-        ))}
-      </div>
-      <div className="carousel_btn">
-        <button onClick={goBack} disabled={currentIndex === 0}>
-          <img src={icon_arrow_left} alt="previous" />
+    <div className="container">
+      <Carousel
+        autoPlay={autoPlay}
+        selectedItem={currentSlide}
+        onChange={updateCurrentSlide}
+        centerMode={true} // Enable center mode
+        centerSlidePercentage={80} // Adjust center slide percentage as needed
+        {...getConfigurableProps()}
+      >
+        {/* Add your carousel items here */}
+        {baseChildren.props.children}
+      </Carousel>
+      <div className="buttons">
+        <button onClick={prev} className="button">
+          <img src={arrowLeft} alt="left" />
         </button>
-        <button
-          onClick={goForward}
-          disabled={currentIndex >= images.length - 3}
-        >
-          <img src={icon_arrow_right} alt="next" />
+        <button onClick={next} className="button">
+          <img src={arrowRight} alt="right" />
         </button>
       </div>
     </div>
   );
 };
 
-export default Carousel;
+export default CenterModeCarousel;
